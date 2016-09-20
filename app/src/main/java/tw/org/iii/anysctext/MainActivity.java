f3c0ca74d71953aff267f95054a4b5d9e2b65243
guiteam <piscesgui01@gmail.com>
2016-09-20 09:54:21 +0800

    v2

--- app/src/main/java/tw/org/iii/anysctext/MainActivity.java
+++ app/src/main/java/tw/org/iii/anysctext/MainActivity.java
@@ -5,19 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private  MyTask mytask;
+    private TextView mesg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mesg =(TextView)findViewById(R.id.mesg);
     }
 
     public  void text1(View v){
         mytask = new MyTask();
-        mytask.execute();
+        mytask.execute("brad","brad","brad","brad");
 
     }
     public  void text2(View v){
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     }
                             //大寫Void 泛型實體物件
                             //非同步
-    private  class  MyTask extends AsyncTask<Void,Void,Void>{
+    private  class  MyTask extends AsyncTask<String,Object,Void>{
                                 @Override
                                 protected void onCancelled(Void aVoid) {
                                     super.onCancelled(aVoid);
@@ -44,14 +46,28 @@ public class MainActivity extends AppCompatActivity {
                                 }
 
                                 @Override
-                                protected void onProgressUpdate(Void... values) {
-                                    Log.d("brad","onProgressUpdate");
+                                protected void onProgressUpdate(Object... values) {
+
                                     super.onProgressUpdate(values);
+                                    Log.d("brad","onProgressUpdate:");
+                                    mesg.setText((Integer)values[0]+":"+(String)values[1]+":"+ values[2]);
                                 }
 
                                 @Override
-                                protected Void doInBackground(Void... params) {
+                                protected Void doInBackground(String... params) {
                                     Log.d("brad","doInBackground");
+
+                                    int i=0 ;
+                                    i++;
+                                    for(String name : params){
+                                        Log.d("brad","Hello,"+name);
+                                        publishProgress(i,name,i+1000);
+                                        try {
+                                            Thread.sleep(1000);
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
                                     return null;
                                 }
 
